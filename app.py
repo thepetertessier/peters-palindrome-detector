76f58aaa2090a134f1983a9b7fa92169702479f2
Peter <fpv5gr@virginia.edu>
2022-09-30 23:07:51 +0200

    Reformat, fix website url, change starting text

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+# Visit http://127.0.0.1:8050/ when running locally
 
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output, State
@@ -7,10 +6,12 @@ from dash.dependencies import Input, Output, State
 # Variables
 tabtitle = 'palindrome detector'
 githublink = 'https://github.com/thepetertessier/peters-palindrome-detector'
-mywebsitelink = 'https://google.com'
+mywebsitelink = 'https://petertessier.com'
+starting_text = "Was it a cat I saw?"
 
 # Helper functions
 def clean_text(text, case_sens, space_sens, punc_sens):
+    # Modify text according to checked parameters
     punctuation_chars = '''!()-[]{};:'"\,<>./?@#$%^&*_~–—…|+='''
     if not case_sens:
         text = text.lower()
@@ -22,6 +23,7 @@ def clean_text(text, case_sens, space_sens, punc_sens):
     return text
 
 def palindrome_test(text, case_sens, space_sens, punc_sens):
+    # Logic for testing if palindrome
     text = clean_text(text, case_sens, space_sens, punc_sens)
     for i, letter in enumerate(text[:(len(text)//2)]):
         if letter != text[-i - 1]:
@@ -31,7 +33,7 @@ def palindrome_test(text, case_sens, space_sens, punc_sens):
 # Initiate app
 app = Dash(__name__)
 server = app.server
-app.title=tabtitle
+app.title = tabtitle
 
 colors = {
     'background': '#111111',
@@ -42,25 +44,21 @@ app.layout = html.Div([
     html.H1(
         children='Palindrome Detector',
         style={
-            'textAlign': 'center',
             'color': colors['theme']
         }
     ),
-
     html.Div(children=[
-        html.H6('Parameters:',style={"size":25}),
+        html.H6('Parameters:'),
         dcc.Checklist(['case sensitive?', 'space sensitive?', 'punctuation sensitive?'],
                        value=[],
                        id='sensitivities'),
         html.Br(),
-        dcc.Input(id='input_text', value='kayak', type='text'),
+        dcc.Input(id='input_text', value=starting_text, type='text'),
         html.Button(children='Go!', id='go_button', n_clicks=0,
                     style={
                     'background-color': colors['theme'],
                     'color': 'white',
-                    'margin-left': '5px',
-                    'verticalAlign': 'center',
-                    'horizontalAlign': 'center'}
+                    'margin-left': '5px'}
                     ),
         html.Br(),
         html.Br(),
@@ -71,9 +69,9 @@ app.layout = html.Div([
         html.Br(),
         html.A('About me', href=mywebsitelink)
     ])
-], style={'textAlign': 'center', 'display': 'flex', 'flex-direction': 'column'})
+], style={'textAlign': 'center'})
 
-
+# If the button is clicked, detect if the inputted text is a palindrome
 @app.callback(
     Output('output-div','children'),
     Input('go_button', 'n_clicks'),
